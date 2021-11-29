cf09245ff64138521094e108a1bb6c0a9b566c1d
Hannah <hannahwl@buffalo.edu>
2021-11-29 11:28:33 -0500

    refactor code

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 import Root from "./components/Root";
-import PieChart from "./Arc";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <PieChart></PieChart> */}
     <Root />
-    {/* <TableApp/> */}
   </React.StrictMode>,
   document.getElementById("root"),
 );
